9aaaa3b8f384aa6f020279e17472f1fdec82d80c
sebby <sebastian.obando@ucas-edu.net>
2024-09-11 20:23:37 +0000

    finished python coding practice

---
+++ variables practice/practice.py
@@ -0,0 +1,20 @@
+name = "Sebastian"
+print(name)
+num110 = "8"
+print(num110)
+num1001000 = "425"
+print(num1001000)
+brkfst = "Pancakes"
+print(brkfst)
+favcolor = "Purple"
+print(favcolor)
+schlname = "UCAS"
+print(schlname)
+year = "2024"
+print(year)
+eyeclr = "Black"
+print(eyeclr)
+age = "14"
+print(age)
+favsub = "Mathematics"
+print(favsub)
