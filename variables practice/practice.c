d39dd7616a616a154ec5bd1f89a4381cd89a35be
sebby <sebastian.obando@ucas-edu.net>
2024-09-20 20:01:21 +0000

    sigma

--- variables practice/practice.c
+++ variables practice/practice.c
@@ -7,6 +7,29 @@ int main(void){
     char num110[] = "9";
     printf("%s \n", num110);
 
-    char 
+    char num1001000[] = "561";
+    printf("%s \n", num1001000);
+
+    char brkfst[] = "Pancakes";
+    printf("%s \n", brkfst);
+    
+    char favclr[] = "Purple";
+    printf("%s \n", favclr);
+
+    char schlname[] = "UCAS";
+    printf("%s \n", schlname);
+
+    char year[] = "2024";
+    printf("%s \n", year);
+    
+    char eyeclr[] = "Black";
+    printf("%s \n", eyeclr);
+
+    char age[] = "14";
+    printf("%s \n", age);
+
+    char favsub[] = "Mathematics";
+    printf("%s \n", favsub);
+
     return 0;
 }
