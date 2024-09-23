b2ae681ac142e70cea397be34f6dd69fbd8a6936
sebby <sebastian.obando@ucas-edu.net>
2024-09-23 20:38:16 +0000

    jwvj

---
+++ expressions/practice.py
@@ -0,0 +1,13 @@
+num = 2
+numTwo = 8
+perensEight = 1*4-2*2
+
+print(7-24//(numTwo)*4+6)
+print(18//3-7+(num)*5)
+print(6*4//12+72//(numTwo)-9)
+print(17-6//(num)+4*3)
+print(-2*(1*4-(num)//(num))+6+(num)-3)
+print(-1*((3-4*7)//5)-(num)*24//6)
+print((3*5^(num)//15)-(5-(num)**(num)) )
+print((1**4*(num)**(num)+3**3)-(num)**5//4 )
+print((22//(num)-(num)*5)**(num)+(4-6//6)**(num) )
