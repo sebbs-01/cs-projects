45edef24b09d65244a3d01365499b5ca0207d054
sebby <sebastian.obando@ucas-edu.net>
2024-09-24 20:41:37 +0000

    mvqed

---
+++ expressions/practice.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <math.h>
+
+
+int main(void){
+    float num = 2, numTwo = 8;
+
+    printf("%f\n", 7-24/(numTwo)*4+6);
+    printf("%f\n", 18/3-7+(num)*5);
+    printf("%f\n", 6*4/12+72/(numTwo)-9);
+    printf("%f\n", 17-6/(num)+4*3);
+    printf("%f\n", -2*(1*4-(num)/(num))+6+(num)-3);
+    printf("%f\n", -1*((3-4*7)/5)-(num)*24/6);
+    printf("%d\n", (3*(int)pow(5,num)/15)-(5-(int)pow(num,num)));
+    printf("%d\n", ((int)pow(1,4)*(int)pow(num,num)+(int)pow(3,3))-(int)pow(num,5)/4);
+    return 0;
+}
