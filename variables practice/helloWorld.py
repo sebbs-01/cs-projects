6875e2af562b44c876caa76a84f9fbb283a31662
sebby <sebastian.obando@ucas-edu.net>
2024-10-22 20:25:22 +0000

    egf

--- variables practice/helloWorld.py
+++ variables practice/helloWorld.py
@@ -1,2 +1,7 @@
-name = input("What is your name? ")
-print ("Hello", name)
+def hello(name):
+    return ("Hello" + name)
+print(hello(" Simar"))
+print(hello(" Kaleb"))
+print(hello(" Ace"))
+print(hello(" Tyler"))
+print(hello(" Frank"))
